5b6af3ecc5f0220b3586b7993af34cae958466e0
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-05-24 02:22:36 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -40,9 +40,8 @@ end
 
 def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  for counter in range 0..99999999999
+  for counter in 1..number_of_times do
     puts phrase
-    break if counter > number_of_times
   end 
 end
 
