982ef722e1a7555dbf577b897f0f55e84fd4c627
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-05-24 02:12:06 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -2,7 +2,9 @@
 
 def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-   (number_of_times).times do 
+   number_of_times
+   loop do
+     number_of_times += 1 
      puts phrase
     end
 end
